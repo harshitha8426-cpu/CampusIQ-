98c9834e69ce02c015480772742b6db6de78fb4a
harshitha8426-cpu <harshitha8426@gmail.com>
2026-02-14 14:37:31 +0530

    main.py

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+
+from fastapi import FastAPI
+from backend.app.services.simulator_service import generate_data
+from backend.app.services.prediction_service import predict_risk
+
+app = FastAPI(title="CampusIQ API")
+
+@app.get("/")
+def home():
+    return {"message": "CampusIQ AI System Running"}
+
+@app.get("/api/dashboard")
+def dashboard():
+    data = generate_data()
+    risk = predict_risk(data["crowd"], data["waste"], data["runtime"])
+    data["risk_level"] = risk
+    return data
